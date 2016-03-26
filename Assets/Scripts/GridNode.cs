6cdd2c221fd34b90e5382fdeb44ec1c6c10eaa4c
Christopher Mooney <ckmooney@usc.edu>
2016-03-26 13:35:07 -0700

    Gridnodes now show range based on set value

--- Assets/Scripts/GridNode.cs
+++ Assets/Scripts/GridNode.cs
@@ -9,6 +9,8 @@ public class GridNode : MonoBehaviour {
     public bool hasBase;
     GameObject pBase;
 
+    public int pylonRange;      //For player power radius
+
 	void Start () {
         gm = GetComponentInParent<GridManager>();
 
@@ -36,17 +38,25 @@ public class GridNode : MonoBehaviour {
         try { adjacent[7] = gm.FindNode(x - 1, y + 1); }
         catch { adjacent[0] = null; }
 
-        if (hasBase) pBase = Instantiate(Resources.Load("Prefabs/PlayerBase", typeof(GameObject)), transform.position, transform.rotation) as GameObject;
+        if (hasBase) {
+            pBase = Instantiate(Resources.Load("Prefabs/PlayerBase", typeof(GameObject)), transform.position, transform.rotation) as GameObject;
+        }
     }
 	
 	void Update () {
-        bool inRange = false;
-	    foreach(GridNode node in adjacent) {
-            if ((node != null) && (node.hasBase)) inRange = true;
+        int maxRange = 0;
+        if (hasBase) maxRange = 3;
+
+        foreach (GridNode node in adjacent) {
+            if ((node != null) && (node.pylonRange > maxRange)) {
+                maxRange = node.pylonRange;
+            }
         }
 
-        if(inRange) GetComponent<Renderer>().material.color = Color.red;
-        else GetComponent<Renderer>().material.color = Color.white;
+        pylonRange = maxRange - 1;
+
+        if (maxRange == 0) GetComponent<Renderer>().material.color = Color.white;
+        else GetComponent<Renderer>().material.color = Color.red;
     }
 
     public void Destroy()
@@ -55,5 +65,6 @@ public class GridNode : MonoBehaviour {
         Destroy(pBase);
         hasBase = false;
         Instantiate(Resources.Load("Particles/Demolish Particles"), transform.position, Quaternion.Euler(-90, 0, 0));
+        pylonRange = 0;
     }
 }
